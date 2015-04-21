09816c0478911df09df67213b77c228836638363
SoundLogic <SoundLogic2236@gmail.com>
2015-04-21 08:38:51 -0500

    Fixed stack size decreation

--- src/main/java/soundlogic/silva/common/item/ItemDwarvenBarrier.java
+++ src/main/java/soundlogic/silva/common/item/ItemDwarvenBarrier.java
@@ -13,7 +13,7 @@ public class ItemDwarvenBarrier extends ItemMod{
 
     public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer p_77659_3_)
     {
-        if (p_77659_3_.capabilities.isCreativeMode)
+        if (!p_77659_3_.capabilities.isCreativeMode)
         	--p_77659_1_.stackSize;
         p_77659_2_.playSoundAtEntity(p_77659_3_, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
 
