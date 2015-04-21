cad92f29a8cec15b21910ab9595b29f5936bbad1
SoundLogic <SoundLogic2236@gmail.com>
2015-04-20 21:02:57 -0500

    Added Dwarf Rock texture

--- src/main/java/soundlogic/silva/common/block/BlockSimple.java
+++ src/main/java/soundlogic/silva/common/block/BlockSimple.java
@@ -1,8 +1,12 @@
 package soundlogic.silva.common.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import soundlogic.silva.client.lib.LibResources;
 import soundlogic.silva.common.Silva;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 
 public class BlockSimple extends Block{
 
@@ -11,4 +15,9 @@ public class BlockSimple extends Block{
 		setCreativeTab(Silva.creativeTab);
 	}
 
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister par1IconRegister) {
+		blockIcon = par1IconRegister.registerIcon(LibResources.PREFIX_MOD+getUnlocalizedName().replaceAll("tile\\.", ""));
+	}
 }
